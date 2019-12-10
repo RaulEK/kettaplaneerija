d37c7998c9ba6329491a206d7893bdc70d38e2d4
RaulEK <kattai.raul@gmail.com>
2019-12-10 17:05:35 +0200

    Updates

---
+++ precache-manifest.7da86d2ad1e278388ddac844fac683da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd893295df03412a3eb601c1eb114fb",
+    "url": "/kettaplaneerija/index.html"
+  },
+  {
+    "revision": "584f4d7db18123e6e9af",
+    "url": "/kettaplaneerija/static/css/main.03115f83.chunk.css"
+  },
+  {
+    "revision": "60326cb6dac6b11354a5",
+    "url": "/kettaplaneerija/static/js/2.0bf80cf2.chunk.js"
+  },
+  {
+    "revision": "584f4d7db18123e6e9af",
+    "url": "/kettaplaneerija/static/js/main.b94b9feb.chunk.js"
+  },
+  {
+    "revision": "6df83f8cb3d266900ac5",
+    "url": "/kettaplaneerija/static/js/runtime-main.3e4abd4a.js"
+  }
+]);
